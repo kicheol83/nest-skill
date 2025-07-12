1ccdd60bb42468058b5a36ad385e63fa74f5e577
kicheol83 <suyikim7@gmail.com>
2025-07-13 05:33:50 +0900

    feat: configure env variables

--- apps/nest-batch/src/main.ts
+++ apps/nest-batch/src/main.ts
@@ -3,6 +3,6 @@ import { NestBatchModule } from './nest-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(NestBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
--- apps/nest-batch/src/nest-batch.module.ts
+++ apps/nest-batch/src/nest-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NestBatchController } from './nest-batch.controller';
 import { NestBatchService } from './nest-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [NestBatchController],
   providers: [NestBatchService],
 })
