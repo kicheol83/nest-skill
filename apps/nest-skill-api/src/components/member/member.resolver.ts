3d13c4bd52479e7577329d2c46e2c2e5cd83176d
kicheol83 <suyikim7@gmail.com>
2025-07-20 02:29:27 +0900

    feat: develop login and signup business logic

--- apps/nest-skill-api/src/components/member/member.resolver.ts
+++ apps/nest-skill-api/src/components/member/member.resolver.ts
@@ -1,7 +1,33 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
+import { Member } from '../../libs/dto/member/member';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 
 @Resolver()
 export class MemberResolver {
 	constructor(private readonly memberService: MemberService) {}
+
+	@Mutation(() => Member)
+	public async signup(@Args('input') input: MemberInput): Promise<Member> {
+		console.log('Mutation: signup');
+		return this.memberService.signup(input);
+	}
+
+	@Mutation(() => Member)
+	public async login(@Args('input') input: LoginInput): Promise<Member> {
+		console.log('Mutation: login');
+		return this.memberService.login(input);
+	}
+
+	@Mutation(() => String)
+	public async updateMember(): Promise<string> {
+		console.log('Mutation: updateMember');
+		return this.memberService.updateMember();
+	}
+
+	@Query(() => String)
+	public async getMember(): Promise<string> {
+		console.log('Mutation: getMember');
+		return this.memberService.getMember();
+	}
 }
--- apps/nest-skill-api/src/components/member/member.service.ts
+++ apps/nest-skill-api/src/components/member/member.service.ts
@@ -1,8 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Message } from '../../libs/enums/common.enum';
+import { MemberStatus } from '../../libs/enums/member.enum';
 
 @Injectable()
 export class MemberService {
-	constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
+	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
+
+	public async signup(input: MemberInput): Promise<Member> {
+		// Hash password
+		try {
+			const result = await this.memberModel.create(input);
+			// Authentication via TOKEN
+			return result;
+		} catch (err) {
+			console.log('Error signup', err.meessage);
+			throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
+		}
+	}
+
+	public async login(input: LoginInput): Promise<Member> {
+		const { memberNick, memberPassword } = input;
+		const response: Member = await this.memberModel
+			.findOne({ memberNick: memberNick })
+			.select('+memberPassword')
+			.exec();
+
+		if (!response || response.memberStatus === MemberStatus.DELETE) {
+			throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
+		} else if (response.memberStatus === MemberStatus.BLOCK) {
+			throw new InternalServerErrorException(Message.BLOCKED_USER);
+		}
+
+		// compare passwords
+		const isMatch = memberPassword === response.memberPassword;
+		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+
+		return response;
+	}
+
+	public async updateMember(): Promise<string> {
+		return 'updateMember executed';
+	}
+
+	public async getMember(): Promise<string> {
+		return 'getMember executed';
+	}
 }
