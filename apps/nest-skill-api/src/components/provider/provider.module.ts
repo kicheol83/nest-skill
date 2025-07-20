eff476f8a669b3b2dc3403ea1286d35eea009ce7
kicheol83 <suyikim7@gmail.com>
2025-07-20 19:06:13 +0900

    feat: modification of provider module

--- apps/nest-skill-api/src/components/provider/provider.module.ts
+++ apps/nest-skill-api/src/components/provider/provider.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ProviderResolver } from './provider.resolver';
+import { ProviderService } from './provider.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
+import ProviderSchema from '../../schemas/Provider.model';
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: 'Provider',
+				schema: ProviderSchema,
+			},
+		]),
+		AuthModule,
+		ViewModule,
+	],
+	providers: [ProviderResolver, ProviderService],
+})
 export class ProviderModule {}
